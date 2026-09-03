2bbf1755602d0e8e9fff8070086a2e5b24e3e5e8
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-03 23:26:08 +0800

    Added: Docs
    I don't wanna write docs, I wanna build it for the machine, so the machine could understand #larp

--- websight/src/core/scenes/MenuScene.ts
+++ websight/src/core/scenes/MenuScene.ts
@@ -1,10 +1,20 @@
 import Phaser from "phaser";
 
+/**
+ * Represents the menu mage scene.
+ * Handles the buttons and routes to Scene
+ * It is initialized, loaded first by the Phaser game
+ *
+ * referenved by main.ts as the first scene
+ */
 export class MenuScene extends Phaser.Scene {
     constructor() {
         super("MenuScene");
     }
 
+    /**
+     * creates the text and buttons
+     */
     create(): void {
         const CENTER_X = this.cameras.main.width / 2;
         const CENTER_Y = this.cameras.main.height / 2;
@@ -21,6 +31,12 @@ export class MenuScene extends Phaser.Scene {
 
     }
 
+    /**
+     * @param x as x coordinate
+     * @param y as y coordinate
+     * @param label as text content
+     * @param onClick as the click handler
+     */
     private createButton(x: number, y: number, label: string, onClick: () => void): void {
         const button = this.add.text(x, y, label, {
             fontSize: '20px',
