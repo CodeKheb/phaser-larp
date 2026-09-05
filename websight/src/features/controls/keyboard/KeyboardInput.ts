6eba218c0f56252be41ac8991db7d0626ddfa290
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-06 07:25:19 +0800

    Changed to JustDown because isDown can be held

--- websight/src/features/controls/keyboard/KeyboardInput.ts
+++ websight/src/features/controls/keyboard/KeyboardInput.ts
@@ -46,7 +46,7 @@ export class KeyboardInput {
     }
 
     get escape() {
-        return this.escapeKey.isDown;
+        return Phaser.Input.Keyboard.JustDown(this.escapeKey);
     }
 
     get interact() {
