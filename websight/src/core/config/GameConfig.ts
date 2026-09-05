51f6a7b234e8c3d56e1396d36a13bd6d1b88c912
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-06 07:58:56 +0800

    CLOUD_Y is unnecessary because clouds are random

--- websight/src/core/config/GameConfig.ts
+++ websight/src/core/config/GameConfig.ts
@@ -8,7 +8,6 @@ export const WorldConfig = {
     WORLD_HEIGHT: 1280,
     GROUND_Y: 2000, // Y-coordinate where the ground platform is placed
     LOGO_Y: 400, // Y-coordinate where the logo is placed
-    CLOUD_Y: 600, // Y-coordinate where the clouds are placed
     CLOUD_AMOUNT: 20, // Amount of cloud spawn
     CLOUD_SPEED: 0.02, // Cloud movement speed in pixels per millisecond
     ZOOM_AMOUNT: 0.55, // Amount of zoom used in the camera
